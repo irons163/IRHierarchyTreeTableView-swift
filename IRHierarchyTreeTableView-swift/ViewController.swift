ba62b2b813b96332aabf1017e9c4088cede3b47f
Phil Chang <phil.chang@yahooinc.com>
2022-06-10 23:33:37 +0800

    Initial Commit

---
+++ IRHierarchyTreeTableView-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IRHierarchyTreeTableView-swift
+//
+//  Created by Phil Chang on 2022/6/10.
+//  Copyright © 2022 Yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
